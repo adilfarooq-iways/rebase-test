fa6a7c891a24023cc0aab66f6fc645e2a5fbc74c
adilfarooq-iways <adil.farooq@i-ways.net>
2025-05-05 15:19:33 +0500

    modified main page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
       <h1>I am main header</h1>
       <h1>I am main</h1>
+      <h1>I am text</h1>
     </div>
   );
 }
